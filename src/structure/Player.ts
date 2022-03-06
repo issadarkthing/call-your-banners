19cbe9a2048532c51e6834388d5d933ce736624a
raziman <raziman@tutanota.com>
2022-03-06 08:16:24 +0800

    add basic classes

---
+++ src/structure/Player.ts
@@ -0,0 +1,8 @@
+
+
+
+export class Player {
+  coins = 10000;
+
+  constructor(public name: string) {}
+}
---
+++ src/structure/Sword.ts
@@ -0,0 +1,6 @@
+import { Player } from "./Player";
+
+
+export class Sword extends Player {
+
+}
