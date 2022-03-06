9f98f2009ea4c4aad4be8c7709523d73d0267ba4
raziman <raziman@tutanota.com>
2022-03-06 08:27:01 +0800

    improve Player class

--- src/structure/Client.ts
+++ src/structure/Client.ts
@@ -1,6 +1,8 @@
 import { CommandManager } from "@jiman24/commandment";
 import { Client as DiscordClient } from "discord.js";
+import Enmap from "enmap";
 
 export class Client extends DiscordClient {
   commandManager = new CommandManager(process.env.PREFIX || "!");
+  players = new Enmap("player");
 }
--- src/structure/Player.ts
+++ src/structure/Player.ts
@@ -1,8 +1,32 @@
-
+import { User } from "discord.js";
+import { client } from "..";
+import { General } from "./General";
+import { Sword } from "./Sword";
 
 
 export class Player {
   coins = 10000;
 
-  constructor(public name: string) {}
+  constructor(
+    public readonly id: string,
+    public name: string,
+    public type: "general" | "sword",
+  ) {}
+
+  fromUser(user: User) {
+    const data = client.players.get(user.id) as Player;
+    let player: Player = new Sword(data.id, data.name, data.type);
+
+    if (data.type === "general") {
+      player = new General(data.id, data.name, data.type);
+    }
+
+    Object.assign(player, data);
+
+    return player;
+  }
+
+  save() {
+    client.players.set(this.id, { ...this });
+  }
 }
