3fddf65f313eba486000a43c3b7fd1c4b97c65ac
raziman <raziman@tutanota.com>
2022-03-06 18:00:45 +0800

    add bal command

---
+++ src/commands/Bal.ts
@@ -0,0 +1,18 @@
+import { Command } from "@jiman24/commandment";
+import { bold } from "@jiman24/discordjs-utils";
+import { Message } from "discord.js";
+import { Player } from "../structure/Player";
+
+
+export default class extends Command {
+  name = "bal";
+  description = "show player's balance";
+
+  async exec(msg: Message) {
+
+
+    const player = Player.fromID(msg.author.id);
+
+    msg.channel.send(`You have a balance of ${bold(player.coins)} coins`);
+  }
+}
