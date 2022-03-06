1efc053bdb9597f3776c48301daafcfd903760ff
raziman <raziman@tutanota.com>
2022-03-06 10:45:25 +0800

    add cooldown time for both General and Sword

--- src/structure/General.ts
+++ src/structure/General.ts
@@ -1,5 +1,7 @@
+import { time } from "@jiman24/discordjs-utils";
 import { Player } from "./Player";
 
 export class General extends Player {
   static readonly MAX = 2;
+  static readonly COOLDOWN = time.HOUR * 3;
 }
--- src/structure/Sword.ts
+++ src/structure/Sword.ts
@@ -1,6 +1,7 @@
+import { time } from "@jiman24/discordjs-utils";
 import { Player } from "./Player";
 
 
 export class Sword extends Player {
-
+  static COOLDOWN = time.HOUR * 8;
 }
