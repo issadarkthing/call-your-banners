6b4dfc8b8766ad4509359f3d6324ed1eb0fd81d3
raziman <raziman@tutanota.com>
2022-03-08 00:27:31 +0800

    set general initial coins

--- src/structure/General.ts
+++ src/structure/General.ts
@@ -3,6 +3,7 @@ import { Player } from "./Player";
 export class General extends Player {
   static readonly MAX = 2;
   COOLDOWN: number;
+  coins: number = 10_000;
   constructor(id: string, name: string, role: "general" | "sword" ) {
     super(id, name, role);
     this.COOLDOWN = 3;
