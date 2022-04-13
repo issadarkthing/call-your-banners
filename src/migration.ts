45b97848a1e5f16d7be08ab4f8c371b2cc875a74
raziman <raziman@tutanota.com>
2022-04-13 23:09:53 +0800

    reset all player to sword

--- src/migration.ts
+++ src/migration.ts
@@ -1,4 +1,6 @@
+import { client } from ".";
 import { Castle } from "./structure/Castle";
+import { Player } from "./structure/Player";
 
 function resetCastle(castle: Castle) {
   const newCastle = new Castle(castle.id, castle.name);
@@ -7,6 +9,16 @@ function resetCastle(castle: Castle) {
 
 resetCastle(Castle.castleA);
 resetCastle(Castle.castleB);
+
+client.players.forEach((_, id) => {
+  const player = Player.fromID(id as string);
+
+  if (!player) return;
+
+  player.role = "sword";
+  player.save();
+
+})
 
 console.log("reset succesfully");
 
